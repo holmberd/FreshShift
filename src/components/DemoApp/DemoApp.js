66f6892da8554fc7304f261e3a51029747746703
holmberd <holmberd@gmail.com>
2020-12-17 09:17:35 -0800

    wip

--- src/components/DemoApp/DemoApp.js
+++ src/components/DemoApp/DemoApp.js
@@ -13,6 +13,7 @@ import DotsIcon from '../../res/dots.svg';
 import AddAppsIcon from '../../res/add-apps.svg';
 import SlackIcon from '../../res/slack.svg';
 import GmailIcon from '../../res/gmail.svg';
+import CloseTabIcon from '../../res/close-tab.svg';
 
 import classes from './demoApp.module.scss';
 
@@ -20,7 +21,98 @@ export default class DemoApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeGroupId: 'group-1'
+      activeGroupId: 'group0',
+      activeGroupType: 'mailbox',
+      entities: {
+        groups: {
+          group0: {
+            id: 'group0',
+            type: 'mailbox',
+            avatar: './will-smith.png',
+            rootTabId: 's0',
+            activeTabId: 's0',
+            tabIds: ['s0', '1', '2', '3'],
+          },
+          group1: {
+            id: 'group1',
+            type: 'mailbox',
+            avatar: './carlton.png',
+            rootTabId: 's1',
+            activeTabId: 's1',
+            tabIds: ['s1'],
+          },
+          group2: {
+            id: 'group2',
+            type: 'mailbox',
+            avatar: './hilary.png',
+            rootTabId: 's2',
+            activeTabId: 's2',
+            tabIds: ['s2'],
+          },
+          group3: {
+            id: 'group3',
+            type: 'app',
+            Icon: SlackIcon,
+            rootTabId: 's3',
+            activeTabId: 's3',
+            tabIds: ['s3'],
+          }
+        },
+        tabs: {
+          's0': {
+            id: 's0',
+            groupId: 'group0',
+            type: 'static',
+            imageUrl: './gmail.png',
+            url: 'https://mail.google.com/mail/will'
+          },
+          's1': {
+            id: 's1',
+            groupId: 'group1',
+            type: 'static',
+            imageUrl: './gmail.png',
+            url: 'https://mail.google.com/mail/carlton'
+          },
+          's2': {
+            id: 's2',
+            groupId: 'group2',
+            type: 'static',
+            imageUrl: './gmail.png',
+            url: 'https://mail.google.com/mail/hilary'
+          },
+          's3': {
+            id: 's3',
+            groupId: 'group3',
+            type: 'static',
+            imageUrl: './gmail.png',
+            url: 'https://slack.com'
+          },
+          '1': {
+            id: '1',
+            groupId: 'group0',
+            type: 'dynamic',
+            imageUrl: './tryshift.png',
+            title: 'The Best Way to Manage All Your Email Accounts - Shift',
+            url: 'https://tryshift.com'
+          },
+          '2': {
+            id: '2',
+            groupId: 'group0',
+            type: 'dynamic',
+            imageUrl: './google.png',
+            title: 'Google',
+            url: 'https://google.com'
+          },
+          '3': {
+            id: '3',
+            groupId: 'group0',
+            type: 'dynamic',
+            imageUrl: './google.png',
+            title: 'Google',
+            url: 'https://google.com'
+          },
+        }
+      }
     }
   }
 
@@ -28,8 +120,55 @@ export default class DemoApp extends React.Component {
     this.setState({ activeGroupId: id });
   }
 
+  handleAddTabClick = (e) => {
+    // TODO: add handler logic.
+  }
+
+  handleOpenTabClick = ({ id, groupId }) => {
+    this.setState(state => {
+      const activeGroup = state.entities.groups[groupId];
+      return {
+        entities: {
+          groups: {
+            ...state.entities.groups,
+            [groupId]: { ...activeGroup, activeTabId: id },
+          },
+          tabs: { ...state.entities.tabs }
+        }
+      };
+    })
+  }
+
+  handleOpenGmailClick = () => {
+    this.setState(state => {
+      const groupId = state.activeGroupId;
+      const activeGroup = state.entities.groups[groupId];
+      return {
+        entities: {
+          groups: {
+            ...state.entities.groups,
+            [groupId]: { ...activeGroup, activeTabId: activeGroup.rootTabId },
+          },
+          tabs: { ...state.entities.tabs }
+        }
+      };
+    })
+  }
+
   render() {
-    const { activeGroupId } = this.state;
+    const { activeGroupId, entities: { groups, tabs }} = this.state;
+    const activeGroup = groups[activeGroupId];
+
+    console.log('activeGroup', activeGroup);
+
+    const activeTabId = activeGroup.activeTabId;
+    const activeTab = tabs[activeTabId];
+
+    console.log('activeTab', activeTab);
+
+    const activeGroupTabs = activeGroup.tabIds
+      .map(id => tabs[id])
+      .filter(tab => tab.type !== 'static')
 
     return (
       <div className={classes.demoApp}>
@@ -40,13 +179,25 @@ export default class DemoApp extends React.Component {
 
             <div className={classes.topbarLeft}>
               <BrowserButtons/>
-              <div className={classes.tabs}></div>
-              <TopbarButton className='add-tab' Icon={AddIcon} />
+              <div className={classes.tabs}>
+                {activeGroupTabs.map(({ id, groupId, url, title }) => (
+                  <Tab
+                    key={id}
+                    id={id}
+                    groupId={groupId}
+                    url={url}
+                    title={title}
+                    isActive={activeTab.id === id}
+                    onOpen={this.handleOpenTabClick}
+                  />
+                ))}
+              <TopbarButton className='add-tab' Icon={AddIcon} onClick={this.handleAddTabClick}/>
+              </div>
             </div>
 
             <div className={classes.topbarRight}>
               <div className={classes.staticButtons}>
-                <TopbarButton className='gmail' Icon={GmailIcon} />
+                { activeGroup.type === 'mailbox' && <TopbarButton className='gmail' Icon={GmailIcon} onClick={this.handleOpenGmailClick} /> }
                 <TopbarButton className='tab-manager' Icon={WebIcon} />
               </div>
             </div>
@@ -62,24 +213,24 @@ export default class DemoApp extends React.Component {
             <div className={classes.sidebarTop}>
               <SidebarButton className='search-button' Icon={SearchIcon} />
               <Avatar
-                id='group-1'
+                id={groups.group0.id}
                 activeGroupId={activeGroupId}
                 color='aliceblue'
-                imageUrl={'./will-smith.png'}
+                imageUrl={groups.group0.avatar}
                 onClick={this.handleSidebarGroupClick}
               />
               <Avatar
-                id='group-2'
+                id={groups.group1.id}
                 activeGroupId={activeGroupId}
                 color='yellow'
-                imageUrl={'./carlton.png'}
+                imageUrl={groups.group1.avatar}
                 onClick={this.handleSidebarGroupClick}
               />
               <Avatar
-                id='group-3'
+                id={groups.group2.id}
                 activeGroupId={activeGroupId}
                 color='red'
-                imageUrl={'./hilary.png'}
+                imageUrl={groups.group2.avatar}
                 onClick={this.handleSidebarGroupClick}
               />
               {/* <Avatar active={false} color='green' imageUrl={'./jeffrey.png'} /> */}
@@ -88,9 +239,9 @@ export default class DemoApp extends React.Component {
             <div className={classes.sidebarBottom}>
               <SidebarButton
                 className='slack-button'
-                id='group-4'
+                id={groups.group3.id}
                 activeGroupId={activeGroupId}
-                Icon={SlackIcon}
+                Icon={groups.group3.Icon}
                 onClick={this.handleSidebarGroupClick}
               >
                 <div className='unread-badge'>
@@ -104,12 +255,41 @@ export default class DemoApp extends React.Component {
           </div>
 
           <div className={classes.content}>
-            <img src='./gmail.png'/>
+            <img src={activeTab.imageUrl}/>
+            {/* <iframe src="http://localhost:8000" width='100%' frameBorder="0" allowfullscreen sandbox></iframe> */}
           </div>
         </div>
       </div>
     );
   }
+}
+
+function Tab({ id, groupId, title, url, isActive, onOpen, onClose }) {
+  const handleOpenTabClick = (e) => {
+    e.preventDefault();
+    onOpen({ id, groupId });
+  }
+
+  const handleCloseTabClick = (e) => {
+    e.preventDefault();
+    onClose({ id, groupId });
+  }
+
+  return (
+    <div className={`${classes.tab} ${isActive ? 'active' : ''}`} onClick={handleOpenTabClick}>
+      <div className={classes.tabFavicon}>
+        <img src={`https://www.google.com/s2/favicons?domain_url=${url}`} />
+      </div>
+      <div className={classes.tabTitle}>{title}</div>
+      <div className={classes.tabClose} onClick={handleCloseTabClick}>
+        <IconButton>
+          <SvgIcon>
+            <CloseTabIcon/>
+          </SvgIcon>
+        </IconButton>
+      </div>
+    </div>
+  );
 }
 
 function ToolbarButtons() {
@@ -132,9 +312,9 @@ function BrowserButtons() {
   );
 }
 
-function TopbarButton({ className = '', Icon }) {
+function TopbarButton({ className = '', Icon, onClick }) {
   return (
-    <div className={`topbar-button ${className}`}>
+    <div className={`topbar-button ${className}`} onClick={onClick}>
       <IconButton>
         <SvgIcon>
           <Icon />
@@ -174,3 +354,4 @@ function Avatar({ className = '', id, activeGroupId, imageUrl, color, onClick })
     </div>
   );
 }
+
